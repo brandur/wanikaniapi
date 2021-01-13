230915f957db1d6ec27e9fa1c33deafcbdf19ae2
Brandur <brandur@brandur.org>
2021-01-12 22:54:11 -0700

    More assignment list parameters

--- assignment.go
+++ assignment.go
@@ -33,14 +33,81 @@ type AssignmentGetParams struct {
 
 type AssignmentListParams struct {
 	*ListParams
-	Levels []int
+	AvailableAfter                 *time.Time
+	AvailableBefore                *time.Time
+	Burned                         *bool
+	Hidden                         *bool
+	IDs                            []ID
+	ImmediatelyAvailableForLessons *bool
+	ImmediatelyAvailableForReview  *bool
+	InReview                       *bool
+	Levels                         []int
+	SRSStages                      []int
+	Started                        *bool
+	SubjectIDs                     []ID
+	Unlocked                       *bool
+	UpdatedAfter                   *time.Time
 }
 
 func (p *AssignmentListParams) EncodeToQuery() string {
 	values := p.encodeToURLValues()
 
+	if p.AvailableAfter != nil {
+		values.Add("available_after", p.AvailableAfter.Format(time.RFC3339))
+	}
+
+	if p.AvailableBefore != nil {
+		values.Add("available_before", p.AvailableBefore.Format(time.RFC3339))
+	}
+
+	if p.Burned != nil {
+		values.Add("burned", strconv.FormatBool(*p.Burned))
+	}
+
+	if p.Hidden != nil {
+		values.Add("hidden", strconv.FormatBool(*p.Hidden))
+	}
+
+	if p.IDs != nil {
+		values.Add("ids", joinIDs(p.IDs, ","))
+	}
+
+	if p.ImmediatelyAvailableForLessons != nil {
+		values.Add("immediately_available_for_lessons",
+			strconv.FormatBool(*p.ImmediatelyAvailableForLessons))
+	}
+
+	if p.ImmediatelyAvailableForReview != nil {
+		values.Add("immediately_available_for_review",
+			strconv.FormatBool(*p.ImmediatelyAvailableForReview))
+	}
+
+	if p.InReview != nil {
+		values.Add("in_review", strconv.FormatBool(*p.InReview))
+	}
+
 	if p.Levels != nil {
 		values.Add("levels", joinInts(p.Levels, ","))
+	}
+
+	if p.SRSStages != nil {
+		values.Add("srs_stages", joinInts(p.SRSStages, ","))
+	}
+
+	if p.Started != nil {
+		values.Add("started", strconv.FormatBool(*p.Started))
+	}
+
+	if p.Unlocked != nil {
+		values.Add("unlocked", strconv.FormatBool(*p.Unlocked))
+	}
+
+	if p.SubjectIDs != nil {
+		values.Add("subject_ids", joinIDs(p.SubjectIDs, ","))
+	}
+
+	if p.UpdatedAfter != nil {
+		values.Add("updated_after", p.UpdatedAfter.Format(time.RFC3339))
 	}
 
 	return values.Encode()
--- param.go
+++ param.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+func Bool(b bool) *bool {
+	return &b
+}
+
 func Int(i int) *int {
 	return &i
 }
