2f4420be9d29cd13352f12befbf471b79688b161
Brandur <brandur@brandur.org>
2021-01-16 00:10:05 -0700

    Convert subject tests

--- subject_test.go
+++ subject_test.go
@@ -1,6 +1,7 @@
 package wanikaniapi_test
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/brandur/wanikaniapi"
@@ -8,21 +9,33 @@ import (
 	assert "github.com/stretchr/testify/require"
 )
 
-func TestSubjectListAndGet(t *testing.T) {
+func TestSubjectList(t *testing.T) {
 	client := wktesting.TestClient()
-	var sampleID wanikaniapi.ID
+	client.RecordMode = true
 
-	{
-		subjects, err := client.SubjectList(&wanikaniapi.SubjectListParams{})
-		assert.NoError(t, err)
-		assert.Greater(t, len(subjects.Data), 0)
+	_, err := client.SubjectList(&wanikaniapi.SubjectListParams{
+		Hidden: wanikaniapi.Bool(true),
+		Levels: []int{1, 2, 3},
+	})
+	assert.NoError(t, err)
 
-		sampleID = subjects.Data[0].ID
-	}
+	req := client.RecordedRequests[0]
+	assert.Equal(t, []byte(nil), req.Body)
+	assert.Equal(t, http.MethodGet, req.Method)
+	assert.Equal(t, "/v2/subjects", req.Path)
+	assert.Equal(t, "hidden=true&levels=1,2,3", wktesting.QueryUnescape(req.Query))
+}
 
-	{
-		subject, err := client.SubjectGet(&wanikaniapi.SubjectGetParams{ID: &sampleID})
-		assert.NoError(t, err)
-		assert.NotNil(t, subject)
-	}
+func TestSubjectGet(t *testing.T) {
+	client := wktesting.TestClient()
+	client.RecordMode = true
+
+	_, err := client.SubjectGet(&wanikaniapi.SubjectGetParams{ID: wanikaniapi.IDPtr(123)})
+	assert.NoError(t, err)
+
+	req := client.RecordedRequests[0]
+	assert.Equal(t, []byte(nil), req.Body)
+	assert.Equal(t, http.MethodGet, req.Method)
+	assert.Equal(t, "/v2/subjects/123", req.Path)
+	assert.Equal(t, "", req.Query)
 }
