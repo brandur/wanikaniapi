6a2862776bebaf64a291cd58ce0865cd273b1b28
Brandur <brandur@brandur.org>
2021-01-16 00:08:45 -0700

    Convert review tests

--- review_test.go
+++ review_test.go
@@ -1,6 +1,7 @@
 package wanikaniapi_test
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/brandur/wanikaniapi"
@@ -8,21 +9,33 @@ import (
 	assert "github.com/stretchr/testify/require"
 )
 
-func TestReviewListAndGet(t *testing.T) {
+func TestReviewList(t *testing.T) {
 	client := wktesting.TestClient()
-	var sampleID wanikaniapi.ID
+	client.RecordMode = true
 
-	{
-		reviews, err := client.ReviewList(&wanikaniapi.ReviewListParams{})
-		assert.NoError(t, err)
-		assert.Greater(t, len(reviews.Data), 0)
+	_, err := client.ReviewList(&wanikaniapi.ReviewListParams{
+		AssignmentIDs: []wanikaniapi.ID{1, 2, 3},
+		SubjectIDs: []wanikaniapi.ID{4, 5, 6},
+	})
+	assert.NoError(t, err)
 
-		sampleID = reviews.Data[0].ID
-	}
+	req := client.RecordedRequests[0]
+	assert.Equal(t, []byte(nil), req.Body)
+	assert.Equal(t, http.MethodGet, req.Method)
+	assert.Equal(t, "/v2/reviews", req.Path)
+	assert.Equal(t, "assignment_ids=1,2,3&subject_ids=4,5,6", wktesting.QueryUnescape(req.Query))
+}
 
-	{
-		review, err := client.ReviewGet(&wanikaniapi.ReviewGetParams{ID: &sampleID})
-		assert.NoError(t, err)
-		assert.NotNil(t, review)
-	}
+func TestReviewGet(t *testing.T) {
+	client := wktesting.TestClient()
+	client.RecordMode = true
+
+	_, err := client.ReviewGet(&wanikaniapi.ReviewGetParams{ID: wanikaniapi.IDPtr(123)})
+	assert.NoError(t, err)
+
+	req := client.RecordedRequests[0]
+	assert.Equal(t, []byte(nil), req.Body)
+	assert.Equal(t, http.MethodGet, req.Method)
+	assert.Equal(t, "/v2/reviews/123", req.Path)
+	assert.Equal(t, "", req.Query)
 }
