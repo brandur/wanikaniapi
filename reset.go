00f201f5def30a8bc2b8f16b028c82338d8902df
Brandur <brandur@brandur.org>
2021-01-19 16:42:59 -0800

    Fix naming + add documentation

--- reset.go
+++ reset.go
@@ -15,12 +15,15 @@ import (
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// ResetGet retrieves a specific reset by its ID.
 func (c *Client) ResetGet(params *ResetGetParams) (*Reset, error) {
 	obj := &Reset{}
 	err := c.request("GET", "/v2/resets/"+strconv.Itoa(int(*params.ID)), params, nil, obj)
 	return obj, err
 }
 
+// ResetList returns a collection of all resets, ordered by ascending
+// CreatedAt, 500 at a time.
 func (c *Client) ResetList(params *ResetListParams) (*ResetPage, error) {
 	obj := &ResetPage{}
 	err := c.request("GET", "/v2/resets", params, nil, obj)
@@ -37,23 +40,35 @@ func (c *Client) ResetList(params *ResetListParams) (*ResetPage, error) {
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// Reset represents a user reset.
+//
+// Users can reset their progress back to any level at or below their current
+// level. When they reset to a particular level, all of the assignments and
+// review_statistics at that level or higher are set back to their default
+// state.
+//
+// Resets contain information about when those resets happen, the starting
+// level, and the target level.
 type Reset struct {
 	Object
 	Data *ResetData `json:"data"`
 }
 
+// ResetData contains core data of Reset.
 type ResetData struct {
-	Confirmed_at  *time.Time `json:"confirmed_at"`
+	ConfirmedAt  *time.Time `json:"confirmed_at"`
 	CreatedAt     time.Time  `json:"created_at"`
 	OriginalLevel int        `json:"original_evel"`
 	TargetLevel   int        `json:"target_level"`
 }
 
+// ResetGetParams are parameters for ResetGet.
 type ResetGetParams struct {
 	Params
 	ID *WKID
 }
 
+// ResetListParams are parameters for ResetList.
 type ResetListParams struct {
 	ListParams
 	Params
@@ -62,6 +77,7 @@ type ResetListParams struct {
 	UpdatedAfter *WKTime
 }
 
+// EncodeToQuery encodes parametes to a query string.
 func (p *ResetListParams) EncodeToQuery() string {
 	values := p.encodeToURLValues()
 
@@ -75,6 +91,7 @@ func (p *ResetListParams) EncodeToQuery() string {
 	return values.Encode()
 }
 
+// ResetPage represents a single page of Resets.
 type ResetPage struct {
 	PageObject
 	Data []*Reset `json:"data"`
