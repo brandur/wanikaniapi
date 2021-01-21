8c00cc613f9f93d40f14c274f2182c78675cc63e
Brandur <brandur@brandur.org>
2021-01-20 23:27:20 -0800

    Add documentation for voice actors

--- voice_actor.go
+++ voice_actor.go
@@ -14,12 +14,15 @@ import (
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// VoiceActorGet retrieves a specific voice_actor by its ID.
 func (c *Client) VoiceActorGet(params *VoiceActorGetParams) (*VoiceActor, error) {
 	obj := &VoiceActor{}
 	err := c.request("GET", "/v2/voice_actors/"+strconv.Itoa(int(*params.ID)), params, nil, obj)
 	return obj, err
 }
 
+// VoiceActorList returns a collection of all voice actors, ordered by
+// ascending CreatedAt, 500 at a time.
 func (c *Client) VoiceActorList(params *VoiceActorListParams) (*VoiceActorPage, error) {
 	obj := &VoiceActorPage{}
 	err := c.request("GET", "/v2/voice_actors", params, nil, obj)
@@ -36,22 +39,27 @@ func (c *Client) VoiceActorList(params *VoiceActorListParams) (*VoiceActorPage, 
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// VoiceActor represents a voice actor used for vocabulary reading
+// pronunciation audio.
 type VoiceActor struct {
 	Object
 	Data *VoiceActorData `json:"data"`
 }
 
+// VoiceActorData contains core data of VoiceActor.
 type VoiceActorData struct {
 	Description string `json:"description"`
 	Gender      string `json:"gender"`
 	Name        string `json:"name"`
 }
 
+// VoiceActorGetParams are parameters for VoiceActorGet.
 type VoiceActorGetParams struct {
 	Params
 	ID *WKID
 }
 
+// VoiceActorListParams are parameters for VoiceActorList.
 type VoiceActorListParams struct {
 	ListParams
 	Params
@@ -60,6 +68,7 @@ type VoiceActorListParams struct {
 	UpdatedAfter *WKTime
 }
 
+// EncodeToQuery encodes parametes to a query string.
 func (p *VoiceActorListParams) EncodeToQuery() string {
 	values := p.encodeToURLValues()
 
@@ -74,6 +83,7 @@ func (p *VoiceActorListParams) EncodeToQuery() string {
 	return values.Encode()
 }
 
+// VoiceActorPage represents a single page of VoiceActors.
 type VoiceActorPage struct {
 	PageObject
 	Data []*VoiceActor `json:"data"`
