30e8c08bffc10205c899076fc06cebb4b8c6955e
Brandur <brandur@brandur.org>
2021-01-20 23:18:33 -0800

    Documentation for summary

--- summary.go
+++ summary.go
@@ -14,6 +14,7 @@ import (
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// SummaryGet retrieves a summary report.
 func (c *Client) SummaryGet(params *SummaryGetParams) (*Summary, error) {
 	obj := &Summary{}
 	err := c.request("GET", "/v2/summary", params, nil, obj)
@@ -30,31 +31,38 @@ func (c *Client) SummaryGet(params *SummaryGetParams) (*Summary, error) {
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// Summary contains currently available lessons and reviews and the reviews
+// that will become available in the next 24 hours, grouped by the hour.
 type Summary struct {
 	Object
 	Data *SummaryData `json:"data"`
 }
 
+// SummaryData contains core data of Summary.
 type SummaryData struct {
 	Lessons       []*SummaryLesson `json:"lessons"`
 	NextReviewsAt *time.Time       `json:"next_reviews_at"`
 	Reviews       []*SummaryReview `json:"reviews"`
 }
 
+// SummaryGetParams are parameters for SummaryGet.
 type SummaryGetParams struct {
 	Params
 }
 
+// SummaryLesson provides a summary about lessons.
 type SummaryLesson struct {
 	AvailableAt time.Time `json:"available_at"`
 	SubjectIDs  []WKID    `json:"subject_ids"`
 }
 
+// SummaryPage represents a single page of Summaries.
 type SummaryPage struct {
 	PageObject
 	Data []*Summary `json:"data"`
 }
 
+// SummaryReview provides a summary about reviews.
 type SummaryReview struct {
 	AvailableAt time.Time `json:"available_at"`
 	SubjectIDs  []WKID    `json:"subject_ids"`
