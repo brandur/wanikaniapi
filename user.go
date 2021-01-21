2ba9948758ec313e5de3119f4324a70c4261e9d1
Brandur <brandur@brandur.org>
2021-01-20 23:23:15 -0800

    Add user documentation

--- user.go
+++ user.go
@@ -14,12 +14,14 @@ import (
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// UserGet returns a summary of user information.
 func (c *Client) UserGet(params *UserGetParams) (*User, error) {
 	obj := &User{}
 	err := c.request("GET", "/v2/user", params, nil, obj)
 	return obj, err
 }
 
+// UserUpdate returns an updated summary of user information.
 func (c *Client) UserUpdate(params *UserUpdateParams) (*User, error) {
 	wrapper := &userUpdateParamsWrapper{Params: params.Params, User: params}
 	obj := &User{}
@@ -37,11 +39,14 @@ func (c *Client) UserUpdate(params *UserUpdateParams) (*User, error) {
 //
 //////////////////////////////////////////////////////////////////////////////
 
+// User returns basic information for the user making the API request,
+// identified by their API token.
 type User struct {
 	Object
 	Data *UserData `json:"data"`
 }
 
+// UserData contains core data of User.
 type UserData struct {
 	CurrentVacationStartedAt *time.Time        `json:"current_vacation_started_at"`
 	WKID                     string            `json:"id"`
@@ -53,10 +58,12 @@ type UserData struct {
 	Username                 string            `json:"username"`
 }
 
+// UserGetParams are parameters for UserGet.
 type UserGetParams struct {
 	Params
 }
 
+// UserPreferences are preferences for a user.
 type UserPreferences struct {
 	DefaultVoiceActorID        WKID   `json:"default_voice_actor_id"`
 	LessonsAutoplayAudio       bool   `json:"lessons_autoplay_audio"`
@@ -66,6 +73,7 @@ type UserPreferences struct {
 	ReviewsDisplaySRSIndicator bool   `json:"reviews_display_srs_indicator"`
 }
 
+// UserSubscription represents a subscription for a user.
 type UserSubscription struct {
 	Active          bool       `json:"active"`
 	MaxLevelGranted int        `json:"max_level_granted"`
@@ -73,11 +81,13 @@ type UserSubscription struct {
 	Type            string     `json:"type"`
 }
 
+// UserUpdateParams are parameters for UserUpdate.
 type UserUpdateParams struct {
 	Params
 	Preferences *UserUpdatePreferencesParams `json:"preferences,omitempty"`
 }
 
+// UserUpdatePreferencesParams are update parameters for user preferences.
 type UserUpdatePreferencesParams struct {
 	DefaultVoiceActorID        *WKID   `json:"default_voice_actor_id,omitempty"`
 	LessonsAutoplayAudio       *bool   `json:"lessons_autoplay_audio,omitempty"`
